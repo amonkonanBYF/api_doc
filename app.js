4df0d0e6ad28bac3f9c8afa24bff199cbea1f2a9
catDev <fabianbrin@gmail.com>
2020-12-17 12:25:49 +0100

    init

---
+++ app.js
@@ -0,0 +1,30 @@
+const Koa = require('koa');
+const app = new Koa();
+const http = require('http');
+
+//logger
+ 
+app.use( async (ctx, next) => {
+	await next();
+	const rt = ctx.response.get('X-Response-Time');
+	console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// response
+
+app.use( async ctx => {
+	
+	ctx.body = 'hello world';
+}); 
+
+http.createServer(app.callback()).listen(3000);
+//app.listen(3000);
